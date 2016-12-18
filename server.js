72ac496cd2d9701fac1c5cb4579d2d1aaca80d76
luii <czarnetzki.philipp@protonmail.ch>
2016-12-18 12:24:25 +0100

    Add documentation for server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
+// Dependencies
 var archy = require('archy')
 
 var pstree = require('./lib')
 
-
+/**
+ * Convert pstree data to display it with archy
+ * @see     https://www.npmjs.com/package/archy
+ * @param   {Object} data `data` should be a tree of nested objects with
+ *                        'label' and 'nodes' fields. 'label' is a string of
+ *                        text to display at a node level and 'nodes' is an
+ *                        array of the descendents of the current node.
+ * @returns {Object}      Return a string representation of obj with unicode
+ *                        pipe characters like how npm ls looks.
+ */
 function pstree2archy(data)
 {
   return Object.keys(data).map(function(pid)
@@ -21,11 +31,12 @@ function pstree2archy(data)
   })
 }
 
-
+// calls pstree function to get a process list
 pstree(function(error, result)
 {
   if(error) throw error
 
+  // converts the process list to archy
   pstree2archy(result).forEach(function(tree)
   {
     console.log(archy(tree))
