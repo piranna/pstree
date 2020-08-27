5faeba25aca4188aee12544ae84dea31a1e9cb73
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-08-27 11:55:50 +0200

    Updated code style & use promises instead of callbacks

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
 #!/usr/bin/env node
 
-'use strict'
-
-// Dependencies
 const archy = require('archy')
-const pstree = require('./lib')
+
+const pstree = require('.')
+
+
+function pid2archy(pid)
+{
+  const {childs, comm: label} = this[pid]
+
+  return {
+    label,
+    nodes: pstree2archy(childs)
+  }
+}
+
 
 /**
  * Convert pstree data to display it with archy
- * @see     https://www.npmjs.com/package/archy
- * @access  private
- * @param   {Object} data `data` should be a tree of nested objects with
- *                        'label' and 'nodes' fields. 'label' is a string of
- *                        text to display at a node level and 'nodes' is an
- *                        array of the descendents of the current node.
- * @returns {Object}      Return a string representation of obj with unicode
- *                        pipe characters like how npm ls looks.
+ *
+ * @see https://www.npmjs.com/package/archy
+ *
+ * @access private
+ *
+ * @param {Object} data `data` should be a tree of nested objects with 'label'
+ *                      and 'nodes' fields. 'label' is a string of text to
+ *                      display at a node level and 'nodes' is an array of the
+ *                      descendents of the current node.
+ *
+ * @returns {Object[]} Array of objects following the `archy` scheme.
  */
 function pstree2archy(data)
 {
-  return Object.keys(data).map(function(pid)
-  {
-    let process = data[pid]
-
-    let result =
-    {
-      label: process.comm,
-      nodes: pstree2archy(process.childs)
-    }
-
-    return result
-  })
+  return Object.keys(data).map(pid2archy, data)
 }
 
-// calls pstree function to get a process list
-pstree(function(error, result)
-{
-  if(error) throw error
 
+// calls pstree function to get a process list
+pstree()
+.then(function(result)
+{
   // converts the process list to archy
-  pstree2archy(result).forEach(function(tree)
-  {
+  for(const tree of pstree2archy(result))
     console.log(archy(tree))
-  })
 })
